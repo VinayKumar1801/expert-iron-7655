125b00b8f50a5e57938e803f993b9d38ce0cd145
VinayKumar1801 <vk2814512@gmail.com>
2022-07-21 23:51:59 +0530

    add day-3 content

---
+++ home.js
@@ -0,0 +1,225 @@
+let scienceArticles = [
+{
+"source": {
+"id": null,
+"name": "The Indian Express"
+},
+"author": "Tech Desk",
+"title": "Watch: NASA shares video showing how Apollo 11 astronauts' tracks are still on the Moon - The Indian Express",
+"description": "Nearly 50 years after Apollo 11's Moon landings, NASA has shared a video which shows that the astronauts' footsteps are still visible on Earth’s satellite.",
+"url": "https://indianexpress.com/article/technology/science/watch-nasa-shares-video-showing-how-apollo-11-astronaut-tracks-still-on-the-moon-8043331/",
+"urlToImage": "https://images.indianexpress.com/2022/07/NASA_Moon_NEW1.jpg",
+"publishedAt": "2022-07-21T09:52:04Z",
+"content": "It has been 50 years since the Apollo 11 landing on the Moon when humans stepped on the surface. Now, in order to commemorate the landing, NASA has shared a video which shows that the astronauts’ foo… [+1658 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Hindustan Times"
+},
+"author": "HT Tech",
+"title": "Big boost for NASA, 7 things James Webb Space Telescope got right tat will reveal secrets of the Universe - HT Tech",
+"description": "NASA James Webb space telescope has been leaving scientists and netizens mesmerized with the stunning view of the universe. Know about these 7 key points of James Webb Space Telescope that will reveal secrets of the universe.",
+"url": "https://tech.hindustantimes.com/tech/news/big-boost-for-nasa-7-things-james-webb-space-telescope-got-right-tat-will-reveal-secrets-of-the-universe-71658394963756.html",
+"urlToImage": "https://images.hindustantimes.com/tech/img/2022/07/21/1600x900/stsci-j-p2223a_1654841042845_1658395087391_1658395087391.jpg",
+"publishedAt": "2022-07-21T09:19:35Z",
+"content": "NASA James Webb Space Telescope was commissioned almost six months ago and it has been doing wonders by capturing stunning views of the universe. The telescope has been giving some major insights int… [+2141 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Tech Explorist"
+},
+"author": "Amit Malewar",
+"title": "First report from the world’s most ambitious sequencing project - Tech Explorist",
+"description": "This is the first report from the largest whole genome sequencing effort to date.",
+"url": "https://www.techexplorist.com/first-report-world-ambitious-sequencing-project/52605/",
+"urlToImage": "https://www.techexplorist.com/wp-content/uploads/2022/07/genome.jpg",
+"publishedAt": "2022-07-21T08:29:34Z",
+"content": "Scientists at deCODE genetics, in collaboration with scientists in Denmark, have released the first report from the greatest whole genome sequencing effort to date. They report on the genome sequence… [+2019 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Hindustan Times"
+},
+"author": "HT Tech",
+"title": "Historic! NASA James Webb Space Telescope finds the oldest galaxy GLASS-z13 - HT Tech",
+"description": "In what is a historic moment, NASA James Webb Space Telescope has found a galaxy called GLASS-z13 that existed 13.5 billion years ago. It is the oldest and most distant galaxy.",
+"url": "https://tech.hindustantimes.com/tech/news/historic-nasa-james-webb-space-telescope-finds-the-oldest-galaxy-glassz13-71658387893088.html",
+"urlToImage": "https://images.hindustantimes.com/tech/img/2022/07/21/1600x900/Untitled_design_-_2022-07-21T123335.791_1658387955238_1658387981778_1658387981778.png",
+"publishedAt": "2022-07-21T07:23:41Z",
+"content": "Space is full of unidentified and undiscovered objects, galaxies, and what not. And now the James Webb Space Telescope has created history by finding a galaxy that existed 13.5 billion years ago. The… [+2543 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Hindustan Times"
+},
+"author": "HT Tech",
+"title": "Shocking! This wild asteroid appeared from nowhere, and headed for Earth; scientists hit by blind spot - HT Tech",
+"description": "An asteroid that appeared from a blind spot and headed for the Earth shocked scientists. The asteroid seems to have come from nowhere. They are now studying the finding of asteroid. Horrifically, Earth was exposed to an unexpected asteroid strike.",
+"url": "https://tech.hindustantimes.com/tech/news/shocking-this-wild-asteroid-appeared-from-nowhere-and-headed-for-earth-scientists-hit-by-blind-spot-71658385922541.html",
+"urlToImage": "https://images.hindustantimes.com/tech/img/2022/07/21/1600x900/asteroid-4145080_1920_1646293428986_1658385991882_1658385991882.jpg",
+"publishedAt": "2022-07-21T06:49:30Z",
+"content": "On July 25, 2019, an asteroid was barrelling towards the Earth but nobody had a clue. As the asteroid 2019 OK approached the Earth, it was only 30 minutes before the asteroid came closest to Earth th… [+2581 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "India Today"
+},
+"author": null,
+"title": "Nasa readies Artemis moon rocket for first flight in late August - India Today",
+"description": "The launch dates could slip, depending on the volatile Florida weather and issues that might arise before the rocket is supposed to return to the pad.",
+"url": "https://www.indiatoday.in/science/story/-nasa-readies-artemis-moon-rocket-for-first-flight-in-late-august-1978135-2022-07-21",
+"urlToImage": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202207/artemis-647x363.jpeg?k6CMDrCm4qcKTADshZG3B5OBLeASeYZa",
+"publishedAt": "2022-07-21T06:23:51Z",
+"content": "On the 53rd anniversary of the Apollo 11 lunar landing, NASA announced Wednesday its shooting for a late August launch of its giant, new moon rocket.\r\nNASA will attempt the more than month-long lunar… [+1943 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "India Today"
+},
+"author": null,
+"title": "Russian, European astronauts to walk out of Space Station, fit robotic arm on flying lab - India Today",
+"description": "The spacewalk will be conducted by Expedition 67 Commander Oleg Artemyev of Roscosmos and European Flight Engineer Samantha Cristoforetti.",
+"url": "https://www.indiatoday.in/science/story/russian-european-astronauts-to-walk-out-of-space-station-fit-robotic-arm-on-flying-lab-1978125-2022-07-21",
+"urlToImage": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202207/Spacewalk_2-647x363.jpeg?kato5Pf4AeozIbeMZTN9xWCKoH.2qJ1h",
+"publishedAt": "2022-07-21T06:04:19Z",
+"content": "Two astronauts will walk out of the safety of the airlock and into the vacuum of space to continue fitting a robotic arm on the International Space Station (ISS). The European and Russian astronauts … [+2092 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Lankatimes.com"
+},
+"author": "Izer",
+"title": "NASA Perseverance Rover: Bundle of Strings Found on Mars - Lanka Times",
+"description": "Sign up for CNN's Wonder Theory newsletter. Explore the universe with news of amazing discoveries, scientific advances, and more.The front-left camera to",
+"url": "https://www.lankatimes.com/nasa-perseverance-rover-bundle-of-strings-found-on-mars/",
+"urlToImage": "https://www.lankatimes.com/wp-content/uploads/2022/07/NASA-Perseverance-Rover-Bundle-of-Strings-Found-on-Mars.jpg",
+"publishedAt": "2022-07-21T05:58:45Z",
+"content": "The front-left camera to avoid hazards in the rover took a photo of a light-colored object on July 12 and some people likened it to spaghetti. \r\nAnd space agency officials emphasized that they believ… [+1360 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Zoom"
+},
+"author": "TN Viral Desk",
+"title": "'Deathpool' at the bottom of famous sea kills anything that swims into it – know why - Times Now",
+"description": "Scientists have discovered a 10-feet long brine pool at the bottom of Red Sea that is lethal to most sea creatures and humans.Rare brine pools in the Red Sea are extremely salty. They cannot sustain marine life forms because they are devoid of any oxygen what…",
+"url": "https://www.timesnownews.com/viral/deathpool-at-the-bottom-of-famous-sea-kills-anything-that-swims-into-it-know-why-article-93019898",
+"urlToImage": "https://static.tnn.in/thumb/msid-93019898,imgsize-100,width-1280,height-720,resizemode-75/93019898.jpg",
+"publishedAt": "2022-07-21T05:44:26Z",
+"content": "Aamir Khan hosts Gujarati dinner for Hollywood directors Russo brothers, Dhanush, ex-wife Kiran Rao joins in"
+},
+{
+"source": {
+"id": null,
+"name": "Daily Mail"
+},
+"author": "Tom Heaton",
+"title": "Wild moment Queensland man plays with stonefish, one of the world's most venomous marine creatures - Daily Mail",
+"description": "Juliano Bayd, 21, came across a stonefish while walking on Stradbroke Island in Queensland and - seemingly unaware of its reputation as the most venomous fish in the world  - touched its mouth.",
+"url": "https://www.dailymail.co.uk/news/article-11034001/Wild-moment-Queensland-man-plays-stonefish-venomous-fish-world.html",
+"urlToImage": "https://i.dailymail.co.uk/1s/2022/07/21/03/60517093-0-image-a-25_1658371721063.jpg",
+"publishedAt": "2022-07-21T05:12:22Z",
+"content": "An unsuspecting young man has come across one of the most venomous fish in the world while on a walk in Queensland. \r\nJuliano Bayd, 21, who says he has a 'strong passion' for marine wildlife and has … [+4199 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "ScienceAlert"
+},
+"author": "Thomas Stewart, The Conversation",
+"title": "One of The First Animals to Venture Onto Land Went Straight Back Into The Water - ScienceAlert",
+"description": "Approximately 365 million years ago, one group of fishes left the water to live on land.",
+"url": "https://www.sciencealert.com/one-of-the-first-animals-to-venture-onto-land-went-straight-back-into-the-water",
+"urlToImage": "https://www.sciencealert.com/images/2022-07/processed/QikiqtaniaDiscovery0_1024.jpg",
+"publishedAt": "2022-07-21T04:59:54Z",
+"content": "Approximately 365 million years ago, one group of fishes left the water to live on land.\r\nThese animals were early tetrapods, a lineage that would radiate to include many thousands of species includi… [+6811 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "Zoom"
+},
+"author": "Times Now Digital",
+"title": "Scientists find a dormant black hole born without a supernova - Times Now",
+"description": "\"It's the first object of its kind discovered after astronomers have been searching for decades,\" said astronomer and study co-author Kareem El-Badry of the Harvard & Smithsonian Center for Astrophysics.",
+"url": "https://www.timesnownews.com/technology-science/scientists-find-a-dormant-black-hole-born-without-a-supernova-article-93008542",
+"urlToImage": "https://static.tnn.in/thumb/msid-93008542,imgsize-100,width-1280,height-720,resizemode-75/93008542.jpg",
+"publishedAt": "2022-07-21T03:38:44Z",
+"content": "India logs 21,566 fresh COVID cases, 45 deaths; active infections rise to 1,48,881"
+},
+{
+"source": {
+"id": null,
+"name": "India.com"
+},
+"author": "Zee Media Bureau",
+"title": "NASA shares stunning rainbow coloured image of Pluto, leaves internet amazed - See pic - Zee News",
+"description": "American Space Agency NASA has taken the internet by storm after it shared a rainbow-coloured image of Pluto on its official Instagram account. NASA keeps on sharing amazing photographs of celestial bodies on its social media platforms.",
+"url": "https://zeenews.india.com/india/nasa-shares-stunning-rainbow-coloured-image-of-pluto-leaves-internet-amazed-see-pic-2487836.html",
+"urlToImage": "https://english.cdn.zeenews.com/sites/default/files/2022/07/21/1067621-untitled-design-2022-07-21t085512.748.jpg",
+"publishedAt": "2022-07-21T03:25:34Z",
+"content": "New Delhi: American Space Agency NASA has taken the internet by storm after it shared a rainbow-coloured image of Pluto on its official Instagram account. NASA keeps on sharing amazing photographs of… [+1064 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "India Education Diary"
+},
+"author": "India Education Diary Bureau Admin",
+"title": "NASA, SpaceX Launch Climate Science Research, More to Space Station - India Education Diary",
+"description": "A SpaceX Dragon resupply spacecraft carrying more than 5,800 pounds of science experiments, crew supplies, and other cargo is on its way to the International Space Station after launching at 8:44 p.m. EDT Thursday from NASA’s Kennedy Space Center in",
+"url": "https://indiaeducationdiary.in/nasa-spacex-launch-climate-science-research-more-to-space-station/",
+"urlToImage": "https://indiaeducationdiary.in/wp-content/uploads/2020/04/NASA-1024x576.jpg",
+"publishedAt": "2022-07-21T02:31:36Z",
+"content": "A SpaceX Dragon resupply spacecraft carrying more than 5,800 pounds of science experiments, crew supplies, and other cargo is on its way to the International Space Station after launching at 8:44 p.m… [+4328 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "India Education Diary"
+},
+"author": "India Education Diary Bureau Admin",
+"title": "Boston Students to Hear from NASA, ESA Astronauts Aboard Space Station - India Education Diary",
+"description": "Massachusetts students will have an opportunity this week to hear from crew members aboard the International Space Station, including a NASA astronaut. The Earth-to-space call will air live at 12:10 p.m. EDT Wednesday, July 20, on NASA Television, th",
+"url": "https://indiaeducationdiary.in/boston-students-to-hear-from-nasa-esa-astronauts-aboard-space-station/",
+"urlToImage": "https://indiaeducationdiary.in/wp-content/uploads/2022/07/jsc2022e011014_01.jpeg",
+"publishedAt": "2022-07-21T02:31:18Z",
+"content": "Massachusetts students will have an opportunity this week to hear from crew members aboard the International Space Station, including a NASA astronaut. The Earth-to-space call will air live at 12:10 … [+1621 chars]"
+},
+{
+"source": {
+"id": null,
+"name": "India Education Diary"
+},
+"author": "India Education Diary Bureau Admin",
+"title": "NASA Awards Launch Services Contract for Roman Space Telescope - India Education Diary",
+"description": "NASA has awarded a NASA Launch Services (NLS) II contract to Space Exploration Technologies Corporation (SpaceX) in Hawthorne, California, to provide launch service for the Nancy Grace Roman Space Telescope mission. The Roman Space Telescope is the t",
+"url": "https://indiaeducationdiary.in/nasa-awards-launch-services-contract-for-roman-space-telescope/",
+"urlToImage": "https://indiaeducationdiary.in/wp-content/uploads/2022/07/trailer_still_1.jpg",
+"publishedAt": "2022-07-21T02:30:10Z",
+"content": "NASA has awarded a NASA Launch Services (NLS) II contract to Space Exploration Technologies Corporation (SpaceX) in Hawthorne, California, to provide launch service for the Nancy Grace Roman Space Te… [+1366 chars]"
+}
+]
+
+console.log(scienceArticles)
+let sci=document.querySelector("#science");
+scienceArticles.forEach(function(el){
+let div = document.createElement("div");
+let image= document.createElement("img");
+image.src=el.urlToImage;
+let para = document.createElement("p");
+para.innerText=el.description;
+let head = document.createElement("h2");
+head.innerText=el.title;
+sci.append(div)
+div.append(image,para)
+console.log(div)
+})
